d9a21859084c8b6ddb557987f1f893acfbbabc1c
idrisAkbarA <idrissmp32@gmail.com>
2020-04-02 14:30:18 +0700

    ready

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<v-container fill-height class="d-flex justify-center">
+    <v-row>
+        <v-col>
+            <h1 class="text-center">Hello</h1>
+        </v-col>
+    </v-row>
+</v-container>
+@endsection
